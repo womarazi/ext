6fd25c0441d5e2a110780535b7032ec1f42e0764
womarazi <44318803+womarazi@users.noreply.github.com>
2024-02-02 17:10:31 +0100

    Update ext.js

--- ext.js
+++ ext.js
@@ -2,6 +2,13 @@ var extmain = null;
 var tohtmltmp = null;
 console.warn('extmain load 5');
 let _extjs_hasExecuted = false;
+function jschanged(prefix, val){
+    injectjscss({key: prefix, innerHTML: val, type:'script', type2:"text/javascript"}});
+}
+function csschanged(prefix, val){
+    injectjscss({key: prefix, innerHTML: val, type:'link', type2:"text/css", rel='stylesheet'}});
+
+}
 function set(key, val){
     if (!key) return;
     try { val =JSON.stringify(val); } catch(e){}
@@ -37,11 +44,11 @@ function extmainstart(){
 
 
     tohtml('<div class="extrow fill">' +
-        toinput({type:'textarea', label: location.host + ' js', key: 'pagejs'}).outerHTML +
-        toinput({type:'textarea', label: location.host + ' css', key: 'pagecss'}).outerHTML + "</div>", true);
+        toinput({type:'textarea', label: location.host + ' js', key: 'pagejs', onchange2: (val)=>jschanged('pagejs', val)}).outerHTML +
+        toinput({type:'textarea', label: location.host + ' css', key: 'pagecss', onchange2: (val)=>csschanged('pagecss', val)}).outerHTML + "</div>", true);
     tohtml('<div class="extrow fill">' +
-        toinput({type:'textarea', label: 'all pages js', key: 'universaljs'}).outerHTML +
-        toinput({type:'textarea', label: 'all pages css', key: 'universalcss'}).outerHTML + "</div>", true);
+        toinput({type:'textarea', label: 'all pages js', key: 'universaljs', onchange2: (val)=>jschanged('universaljs', val)}).outerHTML +
+        toinput({type:'textarea', label: 'all pages css', key: 'universalcss', onchange2: (val)=>csschanged('universalcss', val)}).outerHTML + "</div>", true);
 
     let req = { jq:!!get('jq'), jqui:!!get('jqui')};
     function checkallreqloaded(){
@@ -55,16 +62,35 @@ function extmainstart(){
     if (req.jqui) injectjs('https://code.jquery.com/ui/1.13.2/jquery-ui.min.js',  () => { req.jqui=false; checkallreqloaded(); });
     
     
-}
+}/* cannot be used without key, or it would make duplicate scripts/css files. use the _0 version directly.
 
-function injectjs(src, callback = () => {}, body = undefined) {
-  var script = document.createElement("script");
-  script.src = src; //"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
-  script.onload = function() {
-    if(callback) callback();
+function injectcss_inline(code, callback = () => {}) { return injectjscss_0({innerHTML:code, type:'link', type2:"text/css", rel:'stylesheet'}, callback); }
+
+function injectjs_inline(code, callback = () => {}) { return injectjscss_0({innerHTML:code, type:'script', type2:"text/javascript"}, callback); }*/
+
+function injectjs(src, callback = () => {}) { return injectjscss_src(...arguments); }
+
+function injectjs_src(src, callback = () => {}) { return injectjscss_0({src, type:'script', type2:"text/javascript"}, callback); }
+
+function injectjscss_0(props, callback = () => {}) {
+  var script;
+  if (props.key) script = document.querySelector('#_ext_js_'+key);
+  if (!script) {
+      script = document.createElement(props.type);
+      if (props.key) script.setAttribute('id', '#_ext_js_'+props.key);
+      if (props.src) script.setAttribute('src', props.src); //"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+      if (props.href) script.setAttribute('href', props.href);
+      if (props.type2) script.setAttribute('type', props.type2);
+      if (props.rel) script.setAttribute('rel', props.rel);
+  }
+  if (props.innerHTML) script.innerHTML = props.innerHTML;
+  
+  script.onload = function() { // todo: what if it's already loaded? should i recall the main? like when his code it's updated at runtime.
+    if (callback) callback();
+      main();
     // $("p").css("border", "3px solid red");
   };
-  if (body) script.text = body;
+  if (body) script.text = props.innerHTML;
   console.log("injected js script", {src, body, callback});
   document.head.appendChild(script);
 }
@@ -72,19 +98,35 @@ function injectjs(src, callback = () => {}, body = undefined) {
 function tohtml(str, appendextmain=false, appendbody = false){
     tohtmltmp = tohtmltmp || document.createElement('div');
     tohtmltmp.innerHTML = str;
-    let ret= tohtmltmp.childNodes[0];
+    let ret = tohtmltmp.childNodes[0];
     if(appendbody) document.body.append(ret);
     if(appendextmain) extmain.append(ret);
     return ret;
 }
-function extjs_inputchange(thiss){ console.warn("extjs_inputchange", thiss); let input = thiss; set(input.getAttribute("key"), input.value); }
-function extjs_checkboxchange(thiss){ console.warn("extjs_checkboxchange", thiss); let input = thiss; set(input.getAttribute("key"), input.checked); }
+function extjs_inputchange(thiss){
+    console.warn("extjs_inputchange", {key:input.getAttribute("key"), val: input.value, thiss});
+    let input = thiss;
+    let key = input.getAttribute("key");
+    let inputobj = inputdata[key];
+    set(key, input.value);
+    if (inputobj.onchange2) inputobj.onchange2(key, input.value);
+}
+function extjs_checkboxchange(thiss){
+    console.warn("extjs_checkboxchange", thiss);
+    let input = thiss;
+    set(input.getAttribute("key"), input.checked); }
+var inputdata = {}; // map input key, options
+//let inputid = 0;
+
+
 function toinput(obj/*{min, max, step, type, tag, label...}*/){
     if (!obj) obj = {}
     obj.tag = obj.tag || 'input';
     obj.type = obj.type || 'string';
     obj.label = obj.label || '';
-    console.log( Object.entries(obj));
+    if (!obj.key) console.error('input key is mandatory');
+    inputdata[obj.key] = obj;
+    console.log(Object.entries(obj));
     
     if (obj.value === undefined){
         if (obj.key) obj.value = get(obj.key);
@@ -94,9 +136,8 @@ function toinput(obj/*{min, max, step, type, tag, label...}*/){
 
 
     let innertext = '';
-    
-    obj.onchangefake = "extjs_inputchange(this)";
-    obj.onchange = "extjs_inputchange(this)";
+    obj.onchangefake = "extjs_inputchange(this, " + obj.inputid + ")";
+    obj.onchange = "extjs_inputchange(this" + obj.inputid + ")";
     switch(obj.type){
         default: break;
         case "checkbox":
